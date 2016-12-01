e9abaae37c085fb6dd563fd67f0a9913b170fa94
Anders Orum Veistrup Andersen <ava@avaleo.net>
2016-12-01 15:34:58 +0100

    updated camera control and player control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-    private Vector3 offset;
     public float distance = 2.0f;
+    public float height = 0.2f;
+    private Vector3 defaultDirection;
+    
 
     // Use this for initialization
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        defaultDirection = new Vector3(1.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position - player.GetComponent<Rigidbody>().velocity.normalized * distance;
+        Vector3 direction = player.GetComponent<Rigidbody>().velocity.normalized;
+        if(Mathf.Approximately(direction.x, 0) && Mathf.Approximately(direction.y, 0) && Mathf.Approximately(direction.z, 0))
+        {
+            direction = defaultDirection;
+        }
+
+        Vector3 position = player.transform.position - direction * distance;
+        position.y += height;
+        transform.position = position;
         transform.LookAt(player.transform);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(camera.transform.TransformDirection(movement) * speed);
     }
 
     void OnTriggerEnter(Collider other)
