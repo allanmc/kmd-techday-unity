5d7bbcf16773f5cd326dadd42b33eacffb847174
Allan Mørk Christensen <allanmc@gmail.com>
2016-12-01 12:26:07 +0100

    Added zombie movement

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float speed = 1f;
+    Animator anim;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 movement = new Vector3(0f, 0.0f, 0.0f);
+
+        //rb.AddForce(movement * speed);
+    }
 }
