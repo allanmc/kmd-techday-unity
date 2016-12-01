a7bed508741757e4d2149ef6416dbaaa4bae9bf1
Paul Larsen <pelrsn@hotmail.com>
2016-12-01 13:49:53 +0100

    Implemented win condition.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+    const string PICKUP_TAG = "Pickup";
 
     public float speed;
     public Text scoreText;
+    public Text winText;
 
     private Rigidbody rb;
     private int score;
+    private int totalPickups;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         score = 0;
         UpdateScore();
-	}
+
+        var getCount = GameObject.FindGameObjectsWithTag(PICKUP_TAG);
+        totalPickups = getCount.Length;
+
+        winText.text = "";
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,11 +39,12 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        if (other.gameObject.CompareTag(PICKUP_TAG))
         {
             other.gameObject.SetActive(false);
             score++;
             UpdateScore();
+            CheckWinConditions();
         }
     }
 
@@ -44,4 +53,11 @@ public class PlayerController : MonoBehaviour {
         scoreText.text = "Score: " + score.ToString();
     }
 
+    void CheckWinConditions()
+    {
+        if (score >= totalPickups)
+        {
+            winText.text = "You is Winnar!";
+        }
+    }
 }
