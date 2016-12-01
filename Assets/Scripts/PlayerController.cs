8feb42c34d985235cbdff5985d8a530eef3c300a
Paul Larsen <pelrsn@hotmail.com>
2016-12-01 16:03:23 +0100

    Remove ball when Game is won

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -102,6 +102,7 @@ public class PlayerController : MonoBehaviour {
         if (score >= totalPickups)
         {
             winText.text = "You is Winnar!";
+            this.gameObject.SetActive(false);
         }
     }
 
